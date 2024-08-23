456d8fa777fcc580f94b69573091b7c023ae4aaa
weishiy <118136175+weishiy@users.noreply.github.com>
2024-08-24 09:04:18 +1200

    Deploy to GitHub Pages

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my-resume/',  // 生产环境下使用 GitHub Pages 的路径
+  base: process.env.GITHUB_ACTIONS ? '/my-resume/' : './',
   plugins: [react()],
 });
