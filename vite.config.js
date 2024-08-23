3f9adbf60f35bf56deebe81c6dae3d32ff01e672
weishiy <118136175+weishiy@users.noreply.github.com>
2024-08-23 16:14:46 +1200

    update base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/my-resume/' : './',
+  base: '/my-resume/',
   plugins: [react()],
   css: {
     modules: {
