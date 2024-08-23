f0299f60528d023d0009e69d9672a6ce46eaed6d
weishiy <118136175+weishiy@users.noreply.github.com>
2024-08-23 13:18:18 +1200

    Add build files to dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React_Portfolio_Website/',
+  base: '/my-resume/',
   plugins: [react()],
   css: {
     modules: {
