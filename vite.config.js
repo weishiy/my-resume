d16a369f1fe15b76bd33779b60f0d7c70b45f0e6
weishiy <118136175+weishiy@users.noreply.github.com>
2024-08-26 08:52:24 +1200

    change package.json from private to public

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/my-resume/' : './',
+  base: '/my-resume/',
   plugins: [react()],
   css: {
     modules: {
