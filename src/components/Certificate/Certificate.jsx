2095159b2f5e69b3ae9d94175dcafca55c8a0602
weishiy <118136175+weishiy@users.noreply.github.com>
2024-12-19 15:26:25 +1300

    Add Microsoft Fabric Analytics Engineer Associate certificate

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -4,28 +4,61 @@ import styles from "./Certificate.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Certificate = () => {
+  const certificates = [
+    {
+      title: "AWS Certified Cloud Practitioner",
+      issuer: "Amazon Web Services (AWS)",
+      issued: "Issued Feb 2024",
+      imgSrc: getImageUrl("certificate/aws.png"),
+      imgAlt: "AWS Certificate",
+      url: "https://www.credly.com/badges/ecf7d202-c34b-42d0-b0bd-7ed6ba88faf2/linked_in?t=s8vcr8",
+    },
+    {
+      title: "Microsoft Fabric Analytics Engineer Associate",
+      issuer: "Microsoft",
+      issued: "Issued Dec 2024",
+      imgSrc: getImageUrl("certificate/ms_fabric.png"),
+      imgAlt: "Microsoft Fabric Certificate",
+      url: "https://learn.microsoft.com/en-nz/users/shiyanwei-7293/credentials/22805b75d6b4bf3d?ref=https%3A%2F%2Fwww.linkedin.com%2F",
+    },
+  ];
+
   return (
     <section className={styles.container} id="Certificate">
       <h2 className={styles.title}>Certificates</h2>
       <div className={styles.content}>
-      <div className={styles.imgContainer}>
-          <img
-            src={getImageUrl("certificate/aws.png")}
-            alt="aws ccl"
-            className={styles.certificateImage}
-          />
-        </div>
-        <ul className={styles.CertificateItems} style={{ listStyleType: 'none' }}>
-          <li className={styles.CertificateItem}>
-            <div className={styles.CertificateItemText}>
-              <h3>AWS Certified Cloud Practitioner</h3>
-              <p>
-              Amazon Web Services (AWS)
-              Issued Feb 2024
-              </p>
+        {certificates.map((cert, index) => (
+          <div key={index} className={styles.certItem}>
+            {/* Image with clickable link */}
+            <a
+              href={cert.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.imgLink}
+            >
+              <img
+                src={cert.imgSrc}
+                alt={cert.imgAlt}
+                className={styles.certificateImage}
+              />
+            </a>
+            {/* Text content with clickable title */}
+            <div className={styles.textContainer}>
+              <h3>
+                <a
+                  href={cert.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.certLink}
+                >
+                  {cert.title}
+                </a>
+              </h3>
+              <p>{cert.issuer}</p>
+              <p>{cert.issued}</p>
             </div>
-          </li>
-        </ul>
+          </div>
+        ))}
       </div>
     </section>
   );
