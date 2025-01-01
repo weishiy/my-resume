b2dbf4bea7787f3b26916246aad3a6a164ddb7fa
weishiy <118136175+weishiy@users.noreply.github.com>
2025-01-01 19:00:31 +1300

    bug fix: update the source link in projects section

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -25,7 +25,10 @@ export const ProjectCard = ({
         })}
       </ul>
       <div className={styles.links}>
-        <a href={demo} className={styles.link}>
+      {/* <a href={demo} className={styles.link}>
+        Demo
+        </a> */}
+        <a href={source} className={styles.link}>
         Source
         </a>
 
