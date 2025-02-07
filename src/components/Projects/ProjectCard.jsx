19d3809cd1c839f2222865949516736b65844776
weishiy <118136175+weishiy@users.noreply.github.com>
2025-02-07 16:07:42 +1300

    add a new project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -25,6 +25,9 @@ export const ProjectCard = ({
         })}
       </ul>
       <div className={styles.links}>
+        <a href={demo} className={styles.link}>
+          Demo
+        </a>
         <a href={source} className={styles.link}>
         Source
         </a>
